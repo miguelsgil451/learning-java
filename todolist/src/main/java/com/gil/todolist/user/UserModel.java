bf057b0062a399e0b68581db34e080f10d2502b2
jorelwall154 <jorelwall154@gmail.com>
2025-05-02 19:41:44 -0300

    Update UserModel

--- todolist/src/main/java/com/gil/todolist/user/UserModel.java
+++ todolist/src/main/java/com/gil/todolist/user/UserModel.java
@@ -1,10 +1,28 @@
 package com.gil.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity(name = "users")
 @Data
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
