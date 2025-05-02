801a5fc6e08b4b9a8a1479ca252b9d9c95a08eb4
jorelwall154 <jorelwall154@gmail.com>
2025-05-02 20:03:57 -0300

    Add UserController

---
+++ todolist/src/main/java/com/gil/todolist/user/UserController.java
@@ -0,0 +1,27 @@
+package com.gil.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping
+    public UserModel create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            return null;
+        }
+
+        var createdUser = this.userRepository.save(userModel);
+
+        return createdUser;
+    }
+}
