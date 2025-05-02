6d0d763ee291f330c6af590cdbeb77333126fbed
jorelwall154 <jorelwall154@gmail.com>
2025-05-02 18:43:05 -0300

    initial commit

---
+++ todolist/src/test/java/com/gil/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gil.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
